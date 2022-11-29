7228c5969ea72309b5853a4469a439b8856a1127
cyrus-conde <condecyrus25@gmail.com>
2022-11-30 01:06:39 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+    build:{
+        watch:{
+            plugins: [
+                laravel({
+                    input: [
+                        'resources/sass/app.scss',
+                        'resources/js/app.js',
+                    ],
+                    refresh: true,
+                }),
+                react(),
             ],
-            refresh: true,
-        }),
-        react(),
-    ],
+        }
+    }
+    
 });
