284b190e358d34b20ead16ce080c4e86c0efadd4
cyrus-conde <condecyrus25@gmail.com>
2022-11-30 13:55:18 +0800

    fixed npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,34 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import {resolve} from 'path';
 
 export default defineConfig({
+    publicDir: 'publicest/',
     build:{
         watch:{
+            manifest: true,
+            outDir: 'public/build',
             rollupOptions: {
-                // overwrite default .html entry
-                input: 'resources/js/app.js'
-            },
-            plugins: [
-                laravel({
-                    input: [
-                        'resources/sass/app.scss',
-                        'resources/js/app.js',
-                    ],
-                    refresh: true,
-                }),
-                react(),
-            ],
+                input: ['resources/js/app.jsx','resources/css/app.css'],
+            }
+            
+            
+            
         }
-    }
+        
+        
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.jsx'
+            ],
+            refresh: true,
+            
+        }),
+        react(),
+    ],
     
 });
