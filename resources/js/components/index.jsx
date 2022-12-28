b11e316e4a933f1e11cd7d809a184daf05829e44
cyrus-conde <condecyrus25@gmail.com>
2022-12-28 22:32:28 +0800

    test build

--- resources/js/components/index.jsx
+++ resources/js/components/index.jsx
@@ -11,11 +11,7 @@ const Home = () => {
                 <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 md:grid-rows-2 lg:grid-rows-1">
                     <div className="place-self-center lg:col-span-5 lg:flex">
                         <figure>
-<<<<<<< HEAD
                             <img src={logo} className="h-full object-cover"/>
-=======
-                            <img src={logo} className="md:w-5/12 h-full object-cover"/>
->>>>>>> 2e80bcb9072f2e6cd3b5a0acd70526fcaaedcbdc
                         </figure>
                     </div>
                     <div className="lg:mt-0 lg:col-span-7 lg:flex lg:col-start-8 place-self-center">
